9af217e96facbc00815a05249708979cb847a203
new-nkwaya <naston@gmail.com>
2020-05-06 17:07:18 +0200

    new version

--- print.js
+++ print.js
@@ -2,4 +2,4 @@ console.log('Hello Word')
 console.log('this following program adds two numbers and displays the result on the screen')
 let a = 34, b = 12, c
 c = a + b
-console.log('Addition of a and b is: ' + c )
+console.log('Addition of 34 and 12 is: ' + c )
