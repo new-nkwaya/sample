2601b4a3955582ce23249d35c0134d8b7823d20e
new-nkwaya <naston@gmail.com>
2020-05-06 15:19:30 +0200

    some changes

--- print.js
+++ print.js
@@ -1,1 +1,5 @@
 console.log('Hello Word')
+console.log('this following program adds two numbers and displays the result on the screen')
+let a = 34, b = 12, c
+c = a + b
+console.log('Addition of a and b is: ' + c )
