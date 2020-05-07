e1d688bfbd10f8fee34cdcc7ed3c4312aab24120
new-nkwaya <naston@gmail.com>
2020-05-07 15:32:18 +0200

    added something

--- print.js
+++ print.js
@@ -3,3 +3,12 @@ console.log('this following program adds two numbers and displays the result on 
 let a = 34, b = 12, c
 c = a + b
 console.log('Addition of 34 and 12 is: ' + c )
+
+console.log('the following decides if you are adult or not')
+
+let age = 37
+if (age < 18) {
+    console.log('You are a minor')
+} else {
+    console.log('You are an adult')
+}
